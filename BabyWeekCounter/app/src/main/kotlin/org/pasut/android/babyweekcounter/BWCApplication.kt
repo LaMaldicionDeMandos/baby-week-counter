4f9b8bfc8c0a49784a478ff612a59c16a2ee451a
Marcelo Pasut <pasutmarcelo@gmail.com>
2017-06-20 17:19:07 -0300

    kotlin funcion con expresion y inferencia de tipo

--- BabyWeekCounter/app/src/main/kotlin/org/pasut/android/babyweekcounter/BWCApplication.kt
+++ BabyWeekCounter/app/src/main/kotlin/org/pasut/android/babyweekcounter/BWCApplication.kt
@@ -1,7 +1,6 @@
 package org.pasut.android.babyweekcounter
 
 import android.app.Application
-import android.content.SharedPreferences
 import org.androidannotations.annotations.EApplication
 
 @EApplication
@@ -9,7 +8,5 @@ open class BWCApplication : Application() {
     val DB = "BWC_DB"
     val DATE = "date"
 
-    fun helper(): SharedPreferences {
-        return getSharedPreferences(DB, 4)
-    }
+    fun helper() = getSharedPreferences(DB, 4)
 }
